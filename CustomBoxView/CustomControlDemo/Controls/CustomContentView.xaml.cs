461342480bb73dca8025a4e3935a39ad0aa4097a
bharathirajauk <udhayabharathi888@gmail.com>
2021-04-12 17:27:14 +0530

    Custom box view sample
    Custom box view sample committed

---
+++ CustomBoxView/CustomControlDemo/Controls/CustomContentView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CustomControlDemo.Controls
+{
+    public partial class CustomContentView : ContentView
+    {
+        public CustomContentView()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class MyBoxView : BoxView
+    {
+        public MyBoxView()
+        {
+
+        }
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            return new SizeRequest(new Size(widthConstraint, heightConstraint));
+        }
+    }
+
+}
