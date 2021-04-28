45303cba60503c0ae2eb4c2d3d50aad036307cf0
bharathirajauk <udhayabharathi888@gmail.com>
2021-04-28 17:22:44 +0530

    WPF sample committed

---
+++ ShapesLoading/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageEditorSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestBitmap2ByteArray(new BitmapImage(new Uri(img.Source.ToString())));
+        }
+
+        public static void TestBitmap2ByteArray(BitmapImage bitmap)
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            MemoryStream memstream = new MemoryStream();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            encoder.Save(memstream);
+
+            memstream.Close();
+        }
+    }
+}
